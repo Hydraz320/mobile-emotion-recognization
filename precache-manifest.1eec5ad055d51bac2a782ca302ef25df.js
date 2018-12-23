80f0006782ab8f34d69354e831a467518c843a93
沐飏 <gezhen.gz@alibaba-inc.com>
2018-12-23 17:55:37 +0800

    Updates

---
+++ precache-manifest.1eec5ad055d51bac2a782ca302ef25df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c6b4d69f235731305280",
+    "url": "./static/js/main.c6b4d69f.chunk.js"
+  },
+  {
+    "revision": "7356278b44a7f897a283",
+    "url": "./static/js/1.7356278b.chunk.js"
+  },
+  {
+    "revision": "c6b4d69f235731305280",
+    "url": "./static/css/main.65d3bbd9.chunk.css"
+  },
+  {
+    "revision": "13099d313e87706f4f0da786516310dd",
+    "url": "./index.html"
+  }
+];
