2a9910830147cfc328f240489272cbd6aa4db619
沐飏 <gezhen.gz@alibaba-inc.com>
2018-12-24 17:31:31 +0800

    Updates

---
+++ precache-manifest.0c381f78c622de9eeb83a1e08788d00c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f25016070868274ff0ba",
+    "url": "./static/js/main.f2501607.chunk.js"
+  },
+  {
+    "revision": "7356278b44a7f897a283",
+    "url": "./static/js/1.7356278b.chunk.js"
+  },
+  {
+    "revision": "f25016070868274ff0ba",
+    "url": "./static/css/main.65d3bbd9.chunk.css"
+  },
+  {
+    "revision": "be003c900c59e59134a0f1e914d3be7e",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mobile-emotion-recognization/precache-manifest.e1e27e8f71bb3a706e2c1a879825e674.js"
+  "./precache-manifest.0c381f78c622de9eeb83a1e08788d00c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mobile-emotion-recognization/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
